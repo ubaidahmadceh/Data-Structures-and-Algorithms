2ef3b03adeee29a39364295a71e96a8c5cbcfc73
Ubaid Ahmad <67580747+ubaidahmadceh@users.noreply.github.com>
2022-10-07 12:08:10 +0500

    add

---
+++ patterns.py
@@ -0,0 +1,35 @@
+# outerloop (i) will represent rows
+# innerloop (j) will represent columns
+
+
+#square or rectangle
+
+# for i in range(4):
+#     for j in range(4):
+#         print("* ", end="")
+#     print("")
+
+# --------------------------------------------
+# Triangle
+# for i in range(4):
+#     for j in range(i+1):
+#         print("* ", end="")
+#     print("")
+
+# --------------------------------------------
+# Triangle reverse
+# for i in range(4):
+#     for j in range(4-i):
+#         print("* ", end="")
+#     print("")
+
+#combining upside down
+# row = 4
+# for i in range(row):
+#     for j in range(i+1):
+#         print("* ", end="")
+#     print("")
+# for i in range(row):
+#     for j in range(row-i):
+#         print("* ", end="")
+#     print("")
